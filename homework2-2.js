dc7d55331aa8aa63febae9b222c7708c1d1f2ca0
alexeydukhovich <alexeydukhovich@gmail.com>
2017-03-16 21:49:03 +0300

    added homework 2.2

---
+++ homework2-2.js
@@ -0,0 +1,12 @@
+let arr1 = [1, 3, 5, 7, 9];
+let arr2 = [1, 2, 3, 4];
+
+let arr_with_count = [];
+
+let result = arr1.concat(arr2).filter(
+    function(number) {
+        return (arr_with_count[number] === undefined) ? 
+            !(arr_with_count[number] = 1) : (arr_with_count[number]++ === 1);
+    });
+
+alert(result);
